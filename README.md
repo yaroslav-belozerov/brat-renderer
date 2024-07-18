ca9bb42c1ede2d6777c93fa79dc6af16c9f685fa
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-07-18 20:10:55 +0300

    save feature, readme changes

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # "brat" renderer
 ## draws text on a green square with python & tkinter
 <div style="display: flex">
-    <img src="./misc/startup.png" style="width: 20em"></img>
-    <img src="./misc/slay.png" style="width: 20em"></img>
+    <img src="./misc/brat.png" style="width: 20em"></img>
+    <img src="./misc/tkinter.png" style="width: 20em"></img>
 </div>
 
+### install
+```sh
+pip install -r requirements.txt
+python main.py
+```
 ___
-this is how I relax
+> ### bumpin' that
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
+import os
 from tkinter import ttk
 from tkinter.constants import CENTER
+from tkinter import filedialog as fido
+import pyscreenshot as ImageGrab
 
 BRAT = "#86C800"
 
 def main():
     root = tk.Tk()
-    root.geometry("560x640")
+    root.geometry("680x640")
     frame = ttk.Frame(root, padding=30)
     frame.grid()
     
@@ -27,10 +30,28 @@ def main():
         brat_txt.config(text = txt)
 
     tk.Button(controls, text="Apply", command=lambda: get_text(), bg=BRAT).grid(column=2, row=0, padx=20)
-    tk.Button(controls, text="Quit", command=root.destroy, bg=BRAT).grid(column=3, row=0, padx=20)
+    tk.Button(controls, text="Save", command=lambda: save(), bg=BRAT).grid(column=3, row=0, padx=20)
+    tk.Button(controls, text="Quit", command=root.destroy, bg=BRAT).grid(column=4, row=0, padx=20)
 
+    def save(*event):
+        filename = fido.asksaveasfilename(title = "Create Image", filetypes=(("PNG Image", ".png"),))
+        if filename:
+            if not os.path.exists(filename):
+                path, file = os.path.split(filename)
+                name, ext = os.path.splitext(file)
+                if ext.lower() in ['.gif', '.png']:
+                    x1 = root.winfo_rootx() + brat_frame.winfo_x()
+                    y1 = root.winfo_rooty() + brat_frame.winfo_y()
+                    x2 = x1 + brat_frame.winfo_width()
+                    y2 = y1 + brat_frame.winfo_height()
+                    image = ImageGrab.grab().crop((x1, y1, x2, y2))
+                    image.save(filename)
+                    print(f"Saved image {file}")
+                else:
+                    print("Unknown file type")
+
+    root.bind("<Control-s>", save)
     root.mainloop()
-
 
 
 if (__name__ == "__main__"):
